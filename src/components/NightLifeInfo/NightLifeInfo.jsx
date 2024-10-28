bff9ac5343e2a38cc691d79a2d154d50750920cf
andreasgiannoudis <agiannoudis@hotmail.com>
2024-10-28 18:15:37 +0100

    added photos to the grid in nightlife

--- src/components/NightLifeInfo/NightLifeInfo.jsx
+++ src/components/NightLifeInfo/NightLifeInfo.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import sunset from '/src/assets/img/sunset.jpg';
-
+import museum from '/src/assets/img/museum.jpg';
+import souvlaki from '/src/assets/img/souvlaki.jpg';
 
 function NightLifeInfo() {
   return (
     <div className="nightlife-info">
       <div className="grid-container">
-        <div className="grid-item item1">
+        <Link to="/view" className="grid-item item1">
           <img src={sunset} alt="Sunset in Thessaloniki" />
-        </div>
-        <div className="grid-item item2">
-          <p>A city that inspires</p>
-        </div>
-        <div className="grid-item item3">
-          <p>Delicious food</p>
-        </div>
-        <div className="grid-item item4">
-          <p>Amazing and warm folk</p>
-        </div>
+          <div className="hover-content">
+            <p>Explore the breathtaking views of Thessaloniki’s coastline at sunset.</p>
+          </div>
+        </Link>
+        
+        <Link to="/museums" className="grid-item item2">
+          <img src={museum} alt="Museums in Thessaloniki" />
+          <div className="hover-content">
+            <p>From ancient history to modern art, Thessaloniki offers endless inspiration.</p>
+          </div>
+        </Link>
+        
+        <Link to="/food-drink" className="grid-item item3">
+        <img src={souvlaki} alt="Food in Thessaloniki" />
+
+          <div className="hover-content">
+            <p>Experience the vibrant flavors and welcoming locals of Thessaloniki.</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
